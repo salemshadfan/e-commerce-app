3e918fdcdb6208921dd5ad58c8bdd8151e7ece76
Kosy <kosionyejemezi@gmail.com>
2024-03-25 06:43:13 -0600

    Integrated the Product page into the App.js file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./component/Homepage";
-import Products from "./component/Products"; 
-import Login from "./component/Login"; 
+import Productpage from "./component/Productpage";
+import Login from "./component/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<Productpage />} />
         <Route path="/login" element={<Login />} />
-        
       </Routes>
     </Router>
   );
